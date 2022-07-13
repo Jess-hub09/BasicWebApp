bba98f1a52b5d1d02c6fe8944cccf34b5d05c1b5
Jessica Luu <jessica.luu@tum.de>
2022-07-13 11:31:40 +0200

    Test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -11,12 +11,25 @@ public class QueryProcessor {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
-        } else if (query.contains("name")) {
-           return "MyTeam";
+       /* } else if (query.contains("name")) {
+           return "MyTeam";*/
         //} else { // TODO extend the programm here
 
         } else if(query.contains("name")){
             return "Jessica";
-        }return "";
+        } else if(query.contains("plus")){
+            String[] params = query.split(" ");
+            int sum =0;
+            for(String s : params){
+                try{
+                    int n = Integer.parseInt(s);
+                    sum += n;
+                }catch(NumberFormatException e){
+
+                }
+                return String.valueOf(sum);
+            }
+        }
+        return "";
     }
 }
